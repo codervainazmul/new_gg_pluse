2e0c244b1bf567bc9e2775c2890234db61c5db4f
Nazmul Hossain <olovajs@gmail.com>
2025-05-23 08:19:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Allow CORS from any origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://seraprogrammer.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
